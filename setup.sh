5989ed7e9adabd0e4017e0d50d9c15b22f685e2b
0x1F5 <22313061+0x1F5@users.noreply.github.com>
2024-07-09 00:40:02 +0900

    PX-MLT5PE用に各種スクリプトを調整

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
-cd docker-mirakurun-epgstation
+git clone https://github.com/0x1F5/docker-mirakurun-epgstation-pxmlt5pe.git
+cd docker-mirakurun-epgstation-pxmlt5pe
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.js.template epgstation/config/enc.js
 cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
 cp epgstation/config/serviceLogConfig.sample.yml epgstation/config/serviceLogConfig.yml
-docker-compose run --rm -e SETUP=true mirakurun
+docker compose run --rm -e SETUP=true mirakurun
